545d689ab7065f178e0c6ecdc59782d290f4ecea
Marcos Vinicius Macedo Silva <marcosviniciusid@outlook.com>
2020-10-12 15:56:51 -0300

    aula07-update

--- Aula07-Operadores/index02.js
+++ Aula07-Operadores/index02.js
@@ -7,31 +7,30 @@ relacional
 
 Obs: Pra toda expressão relacional, o resultado sempre vai ser booleano TRUE ou FALSE
 
-5 > 2 = true 
-7 < 4 = false 
-8 >= 8 = true 
-9 <= 7 = false 
-5 == 5 = true 
-4 != 4 = false 
+5 > 2 = true
+7 < 4 = false
+8 >= 8 = true
+9 <= 7 = false
+5 == 5 = true
+4 != 4 = false
 
 
 5 == 5 = true
 5 == '5' = true
 
-Operadores de identidade (mesmo valor, mesmo tipo)
+Operadores de identidade(mesmo valor, mesmo tipo)
 
 5 === '5' = false
 5 === 5 = true
 
 lógicos
 
-!       Negação = Não
-%%      Conjunção = E
-||      Disjunção = Ou
+!   Negação = Não %
+%   Conjunção = E 
+||  Disjunção = Ou
 
 
-!true = false
-!false = true
+!true = false!false = true
 
 true && true = true
 true && false = false
@@ -45,16 +44,12 @@ false || false = false
 
 Ordem de precedencia
 
-() ** /  ...        ARITMETICOS
-> < > = ...          RELACIONAIS 
-!                   LOGICOS
-&& 
-||
+    () ** /  ...        ARITMETICOS >
+    < > = ...RELACIONAIS!LOGICOS &&
+    ||
 
-TERNARIO
+    TERNARIO
 
 teste ? true : false
 
-(media >= 7.0) ? "Aprovado" : "Reprovado"
-
-
+    (media >= 7.0) ? "Aprovado" : "Reprovado"
